b42ae34e147507daacdb436ef7519246a7d8e7b2
Michael Schmidt <michael.schmidt82@gmail.com>
2023-02-04 14:10:06 -0600

    Variable renomed

--- common/ml_utils.py
+++ common/ml_utils.py
@@ -8,12 +8,12 @@ import common.constants as const
 
 def create_fr_encodings() -> dict:
 
-    people = OrderedDict()
+    peoples_faces = OrderedDict()
     face_files = os.listdir(f'{const.PRJ_PATH}/content/people/')
 
     for face in face_files:
         key = face.split('.')[0] # name of person
-        people[key] = fr.load_image_file(f'{const.PRJ_PATH}/content/people/{face}')
-        people[key] = fr.face_encodings(people[key])[0]
+        peoples_faces[key] = fr.load_image_file(f'{const.PRJ_PATH}/content/people/{face}')
+        peoples_faces[key] = fr.face_encodings(peoples_faces[key])[0]
 
-    return people
+    return peoples_faces
